0f8434100a900d3f8955d6ca0b8705838b35b471
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-01-07 23:53:37 +0900

    :see_no_evil: Add __pycache__ to gitignore

--- src/core/config.py
+++ src/core/config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     LEVEL: str
+    DB_NAME: str
     PROJECT_TITLE: str = "동주산업 API 문서"
     PROJECT_VERSION: int = 1
     PROJECT_DESCRIPTION: str = "동주산업 API 문서"
@@ -13,12 +14,10 @@ class Settings(BaseSettings):
         
 class DevelopSettings(Settings):
     DB_URL: str =  Field(env="DEVELOP_DB_URL")
-    DB_NAME: str = Field(env="DEVELOP_DB_NAME")
 
 
 class ProductSettings(Settings):
     DB_URL: str = Field(env="PRODUCT_DB_URL")
-    DB_NMAE: str = Field(env="PRODUCT_DB_NAME")
         
         
 @lru_cache()
