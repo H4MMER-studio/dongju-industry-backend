c1c6d47c119bae9fdb15b9fe47b77e3ef5a38a69
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-03-09 17:36:47 +0900

    :construction: History API

--- src/api/certification.py
+++ src/api/certification.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, status
+from fastapi.responses import JSONResponse
 
 from src.crud import certification_crud  # noqa
 from src.schema import CreateCertification, UpdateCertification  # noqa
@@ -7,12 +8,25 @@ router = APIRouter()
 
 
 @router.get("")
-async def get_certification():
-    pass
+async def get_certification(certification_id: str, request: Request):
+    try:
+        certification_crud.get_one(id=certification_id, request=request)
+
+    except TypeError:
+        return JSONResponse(
+            content={"detail": "{certification_id} Invalid"},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    except Exception as error:
+        return JSONResponse(
+            content={"detail": error},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 @router.get("s")
-async def get_certifications():
+async def get_certifications(request: Request):
     pass
 
 
